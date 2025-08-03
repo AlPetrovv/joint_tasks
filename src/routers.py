6a50c93dee8252321f785904c64c8be32ab924ed
alelsandr <sanya10081999@gmail.com>
2025-08-03 15:52:33 +0700

    Bearer token for auth

--- src/routers.py
+++ src/routers.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.security import HTTPBearer
 
 from announcements.api.handlers import router as announcements_api_router
 from announcements.handlers import form_router as announcements_form_router
@@ -15,30 +16,35 @@ from tasks.api.handlers import router as tasks_api_router
 from tasks.handlers import form_router as tasks_form_router
 from tasks.handlers import router as tasks_router
 
-from profiles.api.handlers import router as users_api_router
-from profiles.handlers import form_router as users_form_router
-from profiles.handlers import router as users_router
+from profiles.api.handlers import router as profiles_api_router
+from profiles.handlers import form_router as profiles_form_router
+from profiles.handlers import router as profiles_router
+
 from users.handlers import auth_router as fastapi_users_auth_router
+from users.handlers import router as api_users_router
+
+http_error = HTTPBearer(auto_error=False)
 
 router = APIRouter(prefix="/api/jt")
 form_router = APIRouter(prefix="/api/jt")
-api_router = APIRouter(prefix="/api")
+api_router = APIRouter(prefix="/api", dependencies=[Depends(http_error)])
 auth_router = APIRouter(prefix=settings.api.prefix)
 
 auth_router.include_router(fastapi_users_auth_router)
 
 form_router.include_router(tasks_form_router)
-form_router.include_router(users_form_router)
+form_router.include_router(profiles_form_router)
 form_router.include_router(folders_form_router)
 form_router.include_router(announcements_form_router)
 
 router.include_router(main_page_router)
 router.include_router(tasks_router)
-router.include_router(users_router)
+router.include_router(profiles_router)
 router.include_router(folders_router)
 router.include_router(announcements_router)
 
 api_router.include_router(tasks_api_router)
-api_router.include_router(users_api_router)
+api_router.include_router(profiles_api_router)
 api_router.include_router(folders_api_router)
 api_router.include_router(announcements_api_router)
+api_router.include_router(api_users_router)
--- src/users/handlers.py
+++ src/users/handlers.py
@@ -3,9 +3,10 @@ from fastapi import APIRouter
 from config import settings
 from users.authentication.fastapi_users import fastapi_users_main_router
 from users.authentication.dependencies.backend import authentication_backend
-from users.schemas import UserRead, UserCreate
+from users.schemas import UserRead, UserCreate, UserUpdate
 
 auth_router = APIRouter(prefix=settings.api.auth, tags=['Auth'])
+router = APIRouter(prefix=settings.api.prefix, tags=['Users'])
 
 # login and logout
 auth_router.include_router(fastapi_users_main_router.get_auth_router(authentication_backend))
@@ -14,3 +15,4 @@ auth_router.include_router(
     fastapi_users_main_router.get_register_router(UserRead, UserCreate),
 )
 
+router.include_router(fastapi_users_main_router.get_users_router(UserRead, UserUpdate))
